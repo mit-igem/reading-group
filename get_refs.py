6b713861147555cec7a9df3991d5d0522e842c72
Matthew Feng <mattfeng@mit.edu>
2023-03-22 18:12:21 -0400

    get refs

--- get_refs.py
+++ get_refs.py
@@ -4,6 +4,23 @@ import argparse
 import json
 import requests
 
+def find_delta(a, b):
+    prefix = []
+    for ca, cb in zip(a, b):
+        if ca == cb:
+            prefix.append(ca)
+        else:
+            break
+    
+    suffix = []
+    for ca, cb in zip(a[::-1], b[::-1]):
+        if ca == cb:
+            suffix.append(ca)
+        else:
+            break
+    
+    return len(prefix), len(suffix)
+
 def get_refs(doi):
     endpoint = f"https://api.crossref.org/works/{doi}"
     r = requests.get(endpoint)
@@ -12,7 +29,18 @@ def get_refs(doi):
     refs = resp["reference"]
     title = resp.get("title", ["no title found"])[0]
 
-    return refs, title
+    plen, slen = find_delta(refs[0]["key"], refs[1]["key"])
+
+    # print(json.dumps(refs, indent=2))
+
+    refs_dict = {}
+
+    for ref in refs:
+        key = ref["key"]
+        refnum = int(key[plen:len(key)-slen])
+        refs_dict[refnum] = ref
+
+    return refs_dict, title
 
 def normalize(refids):
     refids = refids.split(",")
@@ -20,7 +48,7 @@ def normalize(refids):
     normalized = []
     for refid in refids:
         refid = refid.strip()
-        if "-" in refid:
+        if "-" in refid[1:]:
             start, stop = refid.split("-")
             expanded = range(int(start), int(stop) + 1)
             normalized.extend(expanded)
@@ -45,17 +73,13 @@ def main(doi):
         refids = normalize(user_input)
 
         for refid in refids:
-            if refid - 1 >= len(paperrefs):
-                print("refid out of range")
+            if refid not in paperrefs:
+                print(f"{refid}. no title (https://www.doi.org/{doi} ref {refid})")
                 continue
 
-            if refid - 1 < 0:
-                print("refid out of range")
-                continue
-
-            uid = paperrefs[refid - 1].get("DOI")
-            title = paperrefs[refid - 1].get("article-title")
-            unstructured = paperrefs[refid - 1].get("unstructured", "no title")
+            uid = paperrefs[refid].get("DOI")
+            title = paperrefs[refid].get("article-title")
+            unstructured = paperrefs[refid].get("unstructured", "no title")
 
             unstructured = " ".join(map(lambda s: s.strip(), unstructured.split("\n")))
 
